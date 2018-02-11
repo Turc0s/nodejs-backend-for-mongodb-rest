1db2036ebe9fa235459be9851576315d88285d56
tapsa <tapsa872@gmail.com>
2018-02-11 21:02:47 +0200

    Added getGenre and getBooks to display mongodb data in a page

--- nodejs-mongodb-backend/models/genre.js
+++ nodejs-mongodb-backend/models/genre.js
@@ -1,2 +1,21 @@
 
+var mongoose = require("mongoose");
 
+// Genre Schema
+var genreSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    created_date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+var Genre = module.exports = mongoose.model("Genre", genreSchema);
+
+// Get genres
+module.exports.getGenres = (callback, limit) => {
+    Genre.find(callback).limit(limit);
+}
--- nodejs-mongodb-backend/server.js
+++ nodejs-mongodb-backend/server.js
@@ -2,7 +2,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+var mongoose = require("mongoose");
+
+// Connect to Mongoose
+mongoose.connect("mongodb://localhost:23100/bookstore");
+var db = mongoose.connection;
+
 const app = express();
+
+Genre = require("./models/genre");
+Book = require("./models/book");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,7 +21,21 @@ app.get("/", (req, res) => {
 });
 
 app.get("/api/genres", (req, res) => {
+    Genre.getGenres((err, genres) => {
+        if(err) {
+            throw err;
+        }
+        res.json(genres);
+    });
+});
 
+app.get("/api/books", (req, res) => {
+    Book.getBooks((err, books) => {
+        if(err) {
+            throw err;
+        }
+        res.json(books);
+    });
 });
 
 // Set Port
